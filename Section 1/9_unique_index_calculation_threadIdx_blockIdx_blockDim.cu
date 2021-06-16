2a42d52232c9dab50d03ca7f22d8a02cf5b8e162
zheyushen <zheyushen@tencent.com>
2021-06-16 20:45:31 +0800

    Complete Section 1 9_unique_index_calculation_threadIdx_blockIdx_blockDim

--- Section 1/9_unique_index_calculation_threadIdx_blockIdx_blockDim.cu
+++ Section 1/9_unique_index_calculation_threadIdx_blockIdx_blockDim.cu
@@ -19,28 +19,28 @@ __global__ void unique_gid_calculation(int * input)
 		blockIdx.x, tid, gid, input[gid]);
 }
 
-//int main()
-//{
-//	int array_size = 16;
-//	int array_byte_size = sizeof(int) * array_size;
-//	int h_data[] = {23,9,4,53,65,12,1,33,87,45,23,12,342,56,44,99};
-//
-//	for (int i = 0; i < array_size; i++)
-//	{
-//		printf("%d ", h_data[i]);
-//	}
-//	printf("\n \n");
-//
-//	int * d_data;
-//	cudaMalloc((void**)&d_data, array_byte_size);
-//	cudaMemcpy(d_data, h_data, array_byte_size, cudaMemcpyHostToDevice);
-//
-//	dim3 block(4);
-//	dim3 grid(4);
-//	
-//	unique_gid_calculation <<< grid, block >> > (d_data);
-//	cudaDeviceSynchronize();
-//
-//	cudaDeviceReset();
-//	return 0;
-//}
+int main()
+{
+	int array_size = 16;
+	int array_byte_size = sizeof(int) * array_size;
+	int h_data[] = {23,9,4,53,65,12,1,33,87,45,23,12,342,56,44,99};
+
+	for (int i = 0; i < array_size; i++)
+	{
+		printf("%d ", h_data[i]);
+	}
+	printf("\n \n");
+
+	int * d_data;
+	cudaMalloc((void**)&d_data, array_byte_size);
+	cudaMemcpy(d_data, h_data, array_byte_size, cudaMemcpyHostToDevice);
+
+	dim3 block(4);
+	dim3 grid(4);
+	
+	unique_gid_calculation <<< grid, block >> > (d_data);
+	cudaDeviceSynchronize();
+
+	cudaDeviceReset();
+	return 0;
+}
