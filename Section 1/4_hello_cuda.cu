f02f9efc001ecfb4ddf6cea82ea6875f96f04a81
zheyushen <zheyushen@tencent.com>
2021-06-16 20:07:20 +0800

    Complete Section 1 4_hello_cuda

--- Section 1/4_hello_cuda.cu
+++ Section 1/4_hello_cuda.cu
@@ -8,18 +8,18 @@ __global__ void hello_cuda()
 	printf("Hello CUDA world \n");
 }
 
-//int main()
-//{
-//	int nx, ny;
-//	nx = 16;
-//	ny = 4;
-//
-//	dim3 block(8, 2);
-//	dim3 grid(nx / block.x,ny / block.y);
-//
-//	hello_cuda << < grid, block >> > ();
-//	cudaDeviceSynchronize();
-//
-//	cudaDeviceReset();
-//	return 0;
-//}
+int main()
+{
+	int nx, ny;
+	nx = 16;
+	ny = 4;
+
+	dim3 block(8, 2);
+	dim3 grid(nx / block.x,ny / block.y);
+
+	hello_cuda << < grid, block >> > ();
+	cudaDeviceSynchronize();
+
+	cudaDeviceReset();
+	return 0;
+}
