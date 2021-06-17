a38656b62f4dec8fd81c619ef164241f6431daba
zheyushen <zheyushen@tencent.com>
2021-06-17 10:45:00 +0800

    Complete Section 1 10_index calculation for 2D grid

--- Section 1/10_index calculation for 2D grid.cu
+++ Section 1/10_index calculation for 2D grid.cu
@@ -15,22 +15,22 @@ __global__ void unique_gid_calculation_2d(int * data)
 		blockIdx.x, blockIdx.y, tid, gid, data[gid]);
 }
 
-//int main()
-//{
-//	int array_size = 16;
-//	int array_byte_size = sizeof(int) * array_size;
-//	int h_data[] = {23,9,4,53,65,12,1,33,22,43,56,4,76,81,94,32};
-//
-//	int * d_data;
-//	cudaMalloc((void**)&d_data, array_byte_size);
-//	cudaMemcpy(d_data, h_data, array_byte_size, cudaMemcpyHostToDevice);
-//
-//	dim3 block(4);
-//	dim3 grid(2,2);
-//
-//	unique_gid_calculation_2d << < grid, block >> > (d_data);
-//	cudaDeviceSynchronize();
-//
-//	cudaDeviceReset();
-//	return 0;
-//}
+int main()
+{
+	int array_size = 16;
+	int array_byte_size = sizeof(int) * array_size;
+	int h_data[] = {23,9,4,53,65,12,1,33,22,43,56,4,76,81,94,32};
+
+	int * d_data;
+	cudaMalloc((void**)&d_data, array_byte_size);
+	cudaMemcpy(d_data, h_data, array_byte_size, cudaMemcpyHostToDevice);
+
+	dim3 block(4);
+	dim3 grid(2,2);
+
+	unique_gid_calculation_2d << < grid, block >> > (d_data);
+	cudaDeviceSynchronize();
+
+	cudaDeviceReset();
+	return 0;
+}
