45ee020becb44d02d90b59ce7cc99d1889474463
zheyushen <zheyushen@tencent.com>
2021-06-17 10:58:45 +0800

    Complete Section 1 11_index calculation for 2D grid 2D thread blocks

--- Section 1/11_index calculation for 2D grid 2D thread blocks.cu
+++ Section 1/11_index calculation for 2D grid 2D thread blocks.cu
@@ -20,22 +20,22 @@ __global__ void unique_gid_calculation_2d_2d(int * data)
 }
 
 
-//int main()
-//{
-//	int array_size = 16;
-//	int array_byte_size = sizeof(int) * array_size;
-//	int h_data[] = {23,9,4,53,65,12,1,33,22,43,56,4,76,81,94,32};
-//
-//	int * d_data;
-//	cudaMalloc((void**)&d_data, array_byte_size);
-//	cudaMemcpy(d_data, h_data, array_byte_size, cudaMemcpyHostToDevice);
-//
-//	dim3 block(2,2);
-//	dim3 grid(2,2);
-//
-//	unique_gid_calculation_2d_2d << < grid, block >> > (d_data);
-//	cudaDeviceSynchronize();
-//
-//	cudaDeviceReset();
-//	return 0;
-//}
+int main()
+{
+	int array_size = 16;
+	int array_byte_size = sizeof(int) * array_size;
+	int h_data[] = {23,9,4,53,65,12,1,33,22,43,56,4,76,81,94,32};
+
+	int * d_data;
+	cudaMalloc((void**)&d_data, array_byte_size);
+	cudaMemcpy(d_data, h_data, array_byte_size, cudaMemcpyHostToDevice);
+
+	dim3 block(2,2);
+	dim3 grid(2,2);
+
+	unique_gid_calculation_2d_2d << < grid, block >> > (d_data);
+	cudaDeviceSynchronize();
+
+	cudaDeviceReset();
+	return 0;
+}
