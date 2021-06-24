8761689297b3f903de7c3cb3ab8650e11e88a7ad
zheyushen <zheyushen@tencent.com>
2021-06-24 15:32:33 +0800

    Complete Section 2 6_device_query_in_occupancy

--- Section 2/6_device_query_in_occupancy.cu
+++ Section 2/6_device_query_in_occupancy.cu
@@ -3,14 +3,29 @@
 
 #include <stdio.h>
 
-//int main()
-//{
-//	int iDev = 0;
-//	cudaDeviceProp iProp;
-//
-//	cudaGetDeviceProperties(&iProp, iDev);
-//	printf("Max threads per SM : %d \n",
-//		iProp.maxThreadsPerMultiProcessor);
-//
-//	return 0;
-//}
+__global__ void occupany_test(int *results) {
+	int gid = blockDim.x * blockIdx.x + threadIdx.x;
+
+	int x1 = 1;
+	int x2 = 2;
+	int x3 = 3;
+	int x4 = 4;
+	int x5 = 5;
+	int x6 = 6;
+	int x7 = 7;
+	int x8 = 8;
+
+	results[gid] = x1 + x2 + x3 + x4 + x5 + x6 + x7 +x8;
+}
+
+int main()
+{
+	int iDev = 0;
+	cudaDeviceProp iProp;
+
+	cudaGetDeviceProperties(&iProp, iDev);
+	printf("Max threads per SM : %d \n",
+		iProp.maxThreadsPerMultiProcessor);
+
+	return 0;
+}
